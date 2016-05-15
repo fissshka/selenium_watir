18a99d6677eab81e149f94256611e73283407e71
Nataliya Prisych <nataliya.prisych@gmail.com>
2016-05-16 01:23:57 +0300

    login block

---
+++ user_login.rb
@@ -0,0 +1,35 @@
+require 'test/unit'
+require 'selenium-webdriver'
+
+class FirstTest < Test::Unit::TestCase
+  def setup
+    @driver = Selenium::WebDriver.for :firefox
+    @wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+
+  end
+
+  def test_positive_login
+    login_user
+    @wait.until{@driver.find_element(:id, 'loggedas').displayed?}
+    expected = 'Logged in as '+'Zxcvbnm'
+    actual = @driver.find_element(:id, 'loggedas').text
+    assert_equal(expected, actual)
+  end
+  def login_user
+    @driver.navigate.to 'http://demo.redmine.org'
+    @wait.until{@driver.find_element(:class, 'login').displayed?}
+    @driver.find_element(:class, 'login').click
+
+    @wait.until{@driver.find_element(:id, 'username').displayed?}
+    @driver.find_element(:id, 'username').send_keys 'Zxcvbnm'
+    @driver.find_element(:id, 'password').send_keys 'Qwertyu'
+    @driver.find_element(:name, 'login').click
+  end
+
+
+  def teardown
+    @driver.quit
+  end
+
+end
+
